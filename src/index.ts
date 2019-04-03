9b391b0bd9fc96085b707c82ade66812e060a8cd
Fakunle Mayowa Samuel <fakunlesamuel@gmail.com>
2019-04-03 15:04:41 +0100

    (chore): setup webpack with typescript (#1)
    - create webpack config file
    - install webpack and update package.json scripts
    - create sample.env file
    - update index.ts file
    
    [Finishes #165082300]

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000; // default port to listen
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.get("*", (req, res) => {
-  res.send("Welcome to xences");
+app.get('*', (req, res) => {
+  res.send('Welcome to the xences application!!!');
 });
 
 app.listen(process.env.PORT || port);
